ad65db1e33363cb07ef67f65e88cf2ca805f1640
Stian Finjord <stian.finjord@gmail.com>
2025-02-11 11:48:25 +0100

    Complete initial full-stack integration
    Fixed CORS issue, added some test files for frontend and added an api call, so the basic full stack is complete.

---
+++ frontend-vue/vue-project/src/views/MainView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const counter = useCounterStore()
+const templates = ref([])
+
+onMounted(async () => {
+    const response = await axios.get('http://localhost:8080/templates/1')
+    templates.value = response.data
+    console.log('Data fetched: ', templates.value)
+})
+
+
+
+</script>
+
+<template>
+    <main>
+      hallo
+      <h1>Counter: {{ counter.count }}</h1>
+      <h2>Double: {{ counter.doubleCount }}</h2>
+      <button @click="counter.increment">Increment</button>
+
+      <div>
+        <h2>Templates from API:</h2>
+        <pre>{{ templates }}</pre>
+      </div>
+    </main>
+  </template>
